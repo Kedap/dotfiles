cd6d5ff7439d4ea211395d29721cfc3e97b025c2
kedap <kedap.dev@protonmail.com>
2021-08-09 16:39:43 -0500

    new script for gtk

---
+++ tema-gtk/gtk.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/addy-dclxvi/gtk-theme-collections
+cp -r gtk-theme-collections/Fantome ~/.themes
+rm -rf gtk-theme-collections
+git clone https://github.com/vinceliuice/vimix-icon-theme.git
+./vimix-icon-theme/install.sh -a
+rm -rf vimi-icon-theme
+echo "Installing lxappereance"
+sudo pacman -S lxappereance --noconfirm
+lxappereance
