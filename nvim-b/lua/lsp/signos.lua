97670378d7918d4751e88dcc20efd0f362c1fc5b
kedap <kedap.dev@protonmail.com>
2022-03-02 18:43:13 -0600

    actualizando configuracion

--- nvim-b/lua/lsp/signos.lua
+++ nvim-b/lua/lsp/signos.lua
@@ -1,5 +1,13 @@
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = true,
+  serverty_sort = false,
+})
