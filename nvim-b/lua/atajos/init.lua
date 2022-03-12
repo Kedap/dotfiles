17678d956acae3e6fb9f9422beeb142cd94d34f5
kedap <kedap.dev@protonmail.com>
2022-03-12 14:09:31 -0600

    actualizacion de configuracion

--- nvim-b/lua/atajos/init.lua
+++ nvim-b/lua/atajos/init.lua
@@ -7,6 +7,10 @@ map("n", "<C-j>", "<C-w>j", opts)
 map("n", "<C-k>", "<C-w>k", opts)
 map("n", "<C-l>", "<C-w>l", opts)
 
+-- Scroll fast
+map("n", "<C-J>", "50j", opts)
+map("n", "<C-K>", "50k", opts)
+
 -- De cajon
 map("n", "<leader>w", ":w<CR>", opts)
 map("n", "<leader>s", ":x!<CR>", opts)
@@ -37,9 +41,10 @@ map("n", "gr", "<cmd>Lspsaga rename<CR>", opts)
 map("n", "ga", "<cmd>Lspsaga code_action<CR>", opts)
 map("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
 map("n", "<leader>d", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
+map("n", "<C-u>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1, '<c-u>')<cr>", {})
+map("n", "<C-d>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1, '<c-d>')<cr>", {})
 
---Codigo
-map("n", "<leader>ñnf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+-- Codigo
 map("n", "<leader>ñfr", ":FlutterRun<CR>", opts)
 map("n", "<leader>ñfh", ":FlutterHotReload<CR>", opts)
 map("n", "<leader>ñfd", ":FlutterVSplit<CR>", opts)
