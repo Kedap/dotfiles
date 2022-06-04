4c07e49c5602cbb8dd5e0afb325a833d503ad60d
kedap <kedap.dev@protonmail.com>
2022-06-04 16:59:11 -0500

    actualizando configuracion

--- nvim-b/lua/temas/init.lua
+++ nvim-b/lua/temas/init.lua
@@ -12,61 +12,61 @@
 --}
 
 -- Catppuccin
---local catppuccin = require("catppuccin")
---catppuccin.setup()
---vim.cmd([[colorscheme catppuccin]])
---local opciones_lualine = {
---tema = "catppuccin",
---estilo = "burbuja",
---}
+local catppuccin = require("catppuccin")
+catppuccin.setup()
+vim.cmd([[colorscheme catppuccin]])
+local opciones_lualine = {
+  tema = "catppuccin",
+  estilo = "burbuja",
+}
 
 -- configuracion de nightfox
-local nightfox = require("nightfox")
-local grupos = {
-  all = {
-    --TSPunctDelimiter = { fg = "${red}" },
-    LspCodeLens = { bg = "#000000", style = "italic" },
-    DiagnosticUnderlineError = { bg = "NONE", style = "underline" },
-    DiagnosticUnderlineWarn = { bg = "NONE", style = "underline" },
-    DiagnosticUnderlineInfo = { bg = "NONE", style = "underline" },
-    DiagnosticUnderlineHint = { bg = "NONE", style = "underline" },
-  },
-}
-nightfox.setup({
-  options = {
-    styles = {
-      comments = "italic",
-      functions = "NONE",
-      keywords = "bold",
-      numbers = "NONE",
-      strings = "bold",
-      types = "italic,bold",
-      variables = "NONE",
-    },
-    inverse = {
-      match_paren = true,
-    },
-    colors = {
-      red = "#FF000",
-      bg_alt = "#000000",
-    },
-    modules = {
-      lsp_saga = true,
-      cmp = true,
-      hop = true,
-      telescope = true,
-      nvimtree = true,
-      notify = true,
-    },
-  },
-  groups = grupos,
-})
-vim.cmd("colorscheme duskfox")
---vim.cmd("colorscheme terafox")
-local opciones_lualine = {
-  tema = "",
-  estilo = "eviline",
-}
+--local nightfox = require("nightfox")
+--local grupos = {
+--all = {
+----TSPunctDelimiter = { fg = "${red}" },
+--LspCodeLens = { bg = "#000000", style = "italic" },
+--DiagnosticUnderlineError = { bg = "NONE", style = "underline" },
+--DiagnosticUnderlineWarn = { bg = "NONE", style = "underline" },
+--DiagnosticUnderlineInfo = { bg = "NONE", style = "underline" },
+--DiagnosticUnderlineHint = { bg = "NONE", style = "underline" },
+--},
+--}
+--nightfox.setup({
+--options = {
+--styles = {
+--comments = "italic",
+--functions = "NONE",
+--keywords = "bold",
+--numbers = "NONE",
+--strings = "bold",
+--types = "italic,bold",
+--variables = "NONE",
+--},
+--inverse = {
+--match_paren = true,
+--},
+--colors = {
+--red = "#FF000",
+--bg_alt = "#000000",
+--},
+--modules = {
+--lsp_saga = true,
+--cmp = true,
+--hop = true,
+--telescope = true,
+--nvimtree = true,
+--notify = true,
+--},
+--},
+--groups = grupos,
+--})
+--vim.cmd("colorscheme duskfox")
+----vim.cmd("colorscheme terafox")
+--local opciones_lualine = {
+--tema = "",
+--estilo = "eviline",
+--}
 
 -- Configuracion para tokyonight
 --vim.g.tokyonight_style = "storm"
