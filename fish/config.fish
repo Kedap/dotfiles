bd293ff88f33470dea81c105d145aedb9b0968a9
kedap <kedap.dev@protonmail.com>
2022-07-07 19:46:24 -0500

    Modificando la configuracion para fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,10 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# Variables de uso
-set --export EDITOR "nvim"
-
-# Aqui van los alias (heradado desde Zsh)
+# alias para los comandos
 alias v="nvim"
 alias less="bat"
 alias ls="exa"
@@ -16,17 +13,3 @@ alias sdkmanager="sdkmanager --sdk_root=$HOME/etc/android-sdk/"
 alias yarn="yarn --emoji true"
 alias icat="kitty +kitten icat"
 alias dir="exa"
-
-# configurando atajos
-function fish_user_key_bindings
-  bind \c] peco_select_ghq      # Ctrl-]
-  bind \cr peco_select_history  # Ctrl-r
-  bind \cj peco_select_z        # Ctrl-j
-  bind \cf peco_select_file     # Ctrl-f
-end
-
-# configurando el tema
-source ~/.config/fish/conf.d/mocha.fish
-
-# Configurando starship
-starship init fish | source
