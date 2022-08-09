22ab273e0c335155768ca3b5c3a4b7ec6d6e78ff
Kedap <kedap.dev@protonmail.com>
2022-08-09 12:32:03 -0500

    feat(wezterm) agregando configuracion de Wezterm

---
+++ wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require("wezterm")
+return {
+  -- font = wezterm.font("Cascadia Code"),
+  font = wezterm.font("FiraCode Nerd Font"),
+  font_size = 11.0,
+
+  color_scheme = "tokyonight",
+  hide_tab_bar_if_only_one_tab = true,
+  check_for_updates = false,
+  default_cursor_style = "SteadyUnderline",
+  window_background_opacity = 0.85
+}
