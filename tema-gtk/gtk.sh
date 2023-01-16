1075921d7e94b9337f94fff501ba3c8c8a435e6e
Kedap <kedap.dev@protonmail.com>
2023-01-15 20:50:38 -0600

    arrglando la dependencia

--- tema-gtk/gtk.sh
+++ tema-gtk/gtk.sh
@@ -5,5 +5,5 @@ git clone https://github.com/vinceliuice/vimix-icon-theme.git
 ./vimix-icon-theme/install.sh -a
 rm -rf vimi-icon-theme
 echo "Installing lxappereance"
-sudo pacman -S lxappereance --noconfirm
-lxappereance
+sudo pacman -S lxappearance --noconfirm
+lxappearance
