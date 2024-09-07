d9333beeaeb0f6cf6a9068b8e9d840fc93053c38
Kedap <kedap.dev@protonmail.com>
2024-09-07 13:25:29 -0600

    feat(nvim): actualizando config

--- nvim-b/lua/config/mason/servidores.lua
+++ nvim-b/lua/config/mason/servidores.lua
@@ -2,7 +2,7 @@ local lspconfig = require("lspconfig")
 require("mason").setup()
 require("mason-lspconfig").setup({
   -- ensure_installed = { "lua_ls", "typescript-language-server" },
-  ensure_installed = { "lua_ls", "tsserver" },
+  ensure_installed = { "lua_ls" },
 })
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local signature_config = {
@@ -36,16 +36,6 @@ require("mason-lspconfig").setup_handlers({
           },
         },
       },
-    })
-  end,
-  ["tsserver"] = function()
-    require("lspconfig")["tsserver"].setup({
-      capabilities = capabilities,
-      single_file_support = false,
-      root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", "jsconfig.json"),
-      on_attach = function(client, bufnr)
-        require("lsp_signature").on_attach(signature_config, bufnr) -- Note: add in lsp client on-attach
-      end,
     })
   end,
   ["denols"] = function()
