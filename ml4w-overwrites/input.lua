4c0678b09e84656d5532f173449b1b073f08e51e
Kedap <kedap.dev@protonmail.com>
2026-06-13 12:20:45 -0600

    feat(ml4w): agregar sobrescrituras de ml4w

---
+++ ml4w-overwrites/input.lua
@@ -0,0 +1,23 @@
+-- -----------------------------------------------------
+-- Input
+-- -----------------------------------------------------
+
+hl.config({
+    input = {
+        kb_layout  = "latam",
+        kb_variant = "",
+        -- kb_layout  = "es",
+        -- kb_variant = "dvorak",
+        kb_model   = "",
+        kb_options = "",
+        kb_rules   = "",
+
+        follow_mouse = 1,
+
+        sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
+
+        touchpad = {
+            natural_scroll = false,
+        },
+    },
+})
